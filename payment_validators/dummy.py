b7f839eb40ec426dafa859856404e9266f43537a
Maarten Menheere <m.menheere@m2tec.nl>
2022-02-08 15:23:41 +0100

    printing paper wallet

--- payment_validators/dummy.py
+++ payment_validators/dummy.py
@@ -1,7 +1,7 @@
 # Fake check for testing purposes
 def payment_validate(network_type, transaction_id, wallet_address, requested_amount): 
         
-        #payment_status = "not_received"
-        payment_status = "success"
+        payment_status = "not_received"
+        #payment_status = "success"
 
         return payment_status 
