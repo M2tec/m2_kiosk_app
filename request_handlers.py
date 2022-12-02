f1b069a94b654d6103cb609310c48cdaf43ebc00
Maarten Menheere <m.menheere@m2tec.nl>
2022-12-02 11:43:35 +0100

    Add cutomer tip

--- request_handlers.py
+++ request_handlers.py
@@ -43,6 +43,21 @@ def payment_request(server, message, path, query, client_context, data):
         print('json.decoder.JSONDecodeError')
         return
 
+    payment_request_simple(server, json_dict)
+    
+    # HTTP Response
+    cors_local_url = check_cors_origin(message)
+    #print("Cors_local_url: " + cors_local_url)
+    message.response_headers.append("Access-Control-Allow-Origin", cors_local_url)
+    message.set_status(200)
+
+def payment_request_simple(server, json_dict):
+    print("payment_request_simple")
+    
+    server.window1.remove(server.start_main_box)
+    server.window1.add(server.payment_main_box)
+    server.window1.show_all()
+
     transaction_id = str(json_dict["transaction_id"])
     requested_amount = float(json_dict["amount"])
     wallet_address = json_dict["wallet_address"]
@@ -54,14 +69,7 @@ def payment_request(server, message, path, query, client_context, data):
     # Update interface
     set_payment_image(server.payment_image, qr_code_file_name)
     server.payment_label.set_text("{:.2f}".format(requested_amount))  
-    server.payment_box.show()
-
-    # HTTP Response
-    cors_local_url = check_cors_origin(message)
-    #print("Cors_local_url: " + cors_local_url)
-    message.response_headers.append("Access-Control-Allow-Origin", cors_local_url)
-    message.set_status(200)
-
+    server.window1.show_all()
 
 # Handle payment status for the gamechanger wallet
 def payment_status(server, message, path, query, client_context, data):
