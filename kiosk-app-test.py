7de4915b0c8273e083145d0a8d628b23fa810717
Maarten Menheere <m.menheere@m2tec.nl>
2023-01-19 10:18:51 +0100

    Add GPLv3 license headers

--- kiosk-app-test.py
+++ kiosk-app-test.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+
+#
+# This file is part of the m2-kiosk-app distribution (https://github.com/M2tec/m2_kiosk_app).
+# Copyright (c) 2023 Maarten Menheere.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
 import requests
 
 url = "http://localhost:9090/payment-request"
